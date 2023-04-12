c36f35b61c23982ea029617edd04a247766fa4a7
NardeenMahfouzFakhrey <1900586@eng.asu.edu.eg>
2023-04-13 01:09:20 +0200

    FCFS

--- OS_Project/src/main/java/com/example/os_project/FCFS.java
+++ OS_Project/src/main/java/com/example/os_project/FCFS.java
@@ -3,16 +3,33 @@ package com.example.os_project;
 import java.util.ArrayList;
 
 public class FCFS extends  Algorithm{
+
+    ArrayList<Burst> burst = new ArrayList<Burst>();
+    ArrayList<Process> process;
+    int number_of_process;
     float avg_waiting_time = 0;
     float avg_turnaround_time = 0;
+
     public FCFS(int pn, ArrayList<Process> ps) {
         super(pn, ps);
+        this.process=ps;
+        this.number_of_process=pn;
     }
-
     @Override
     public ArrayList<Burst> schedule(String algo_Type){
-
-        return null;
+        int current_time=0;
+        for (int i=0 ; i<number_of_process ; i++) {
+            Process p = getFirstArrival(process);
+            Burst b = new Burst(p,p.getBt());
+            burst.add(b);
+            avg_waiting_time += current_time-p.getAr();
+            current_time = current_time + p.getBt();
+            avg_turnaround_time += current_time-p.getAr();
+            process.remove(p);
+        }
+        avg_turnaround_time=avg_turnaround_time/number_of_process;
+        avg_waiting_time=avg_waiting_time/number_of_process;
+        return burst;
     }
 
     @Override
@@ -24,4 +41,18 @@ public class FCFS extends  Algorithm{
     public float compute_avgta() {
         return avg_turnaround_time;
     }
+
+    public Process getFirstArrival(ArrayList<Process> processes) {
+        int min= Integer.MAX_VALUE;
+        Process minProcess=null;
+        for(int i=0 ; i< processes.size() ; i++) {
+
+            if(processes.get(i).getAr() < min ){
+                min=processes.get(i).getAr();
+                minProcess=processes.get(i);
+            }
+        }
+        return minProcess;
+    }
+
 }
