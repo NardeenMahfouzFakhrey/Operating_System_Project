613277a9e0943c946503a1873af698e335c46299
MariamRaouf30 <mariam.shenouda303@gmail.com>
2023-04-12 16:41:08 +0200

    GUI

--- Operating_System_Project/src/main/java/com/example/operating_system_project/HelloApplication.java
+++ Operating_System_Project/src/main/java/com/example/operating_system_project/HelloApplication.java
@@ -1,18 +1,61 @@
 package com.example.operating_system_project;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+
 public class HelloApplication extends Application {
+
+    private Button buttonOK;
+    private Label labelScheduler;
+    private Label labelProcess;
+    private Label labelTitle;
+    private TextField textProcess;
+    private ComboBox comboBoxSchedulers;
+    private String [] schedulers;
+
+    private FlowPane flowPane;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        schedulers = new String[]{"FCFS","SJF Preemptive", "SJF Non Preemptive" , "Priority Preemptive","Priority Non Preemptive","Round Robin"};
+        buttonOK=new Button("OK");
+        FlowPane flowPane1 = new FlowPane(buttonOK);
+        labelScheduler = new Label("SchedulerType");
+        labelScheduler.setFont(Font.font("Arial",FontWeight.BOLD,14));
+        labelProcess = new Label("Number of processes");
+        labelProcess.setFont(Font.font("Arial",FontWeight.BOLD,14));
+        labelTitle = new Label("Scheduler");
+        labelTitle.setTextFill(Color.DARKRED);
+        labelTitle.setFont(Font.font("Verdana" , FontWeight.BOLD , FontPosture.ITALIC, 28));
+        flowPane = new FlowPane(labelTitle);
+        flowPane.setAlignment(Pos.CENTER);
+        textProcess=new TextField();
+        comboBoxSchedulers = new ComboBox<>(FXCollections.observableArrayList(schedulers));
+        HBox hbox = new HBox(labelScheduler,labelProcess);
+        hbox.setSpacing(30);
+        hbox.setStyle("-fx-padding: 30;");
+        HBox hbox1 = new HBox(comboBoxSchedulers,textProcess);
+        hbox1.setSpacing(25);
+        VBox vbox = new VBox(flowPane,hbox,hbox1,flowPane1);
+        Scene scene = new Scene(vbox, 550, 400);
+        stage.setTitle("Scheduler Project");
         stage.setScene(scene);
         stage.show();
     }
