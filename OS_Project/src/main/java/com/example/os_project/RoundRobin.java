963e1a66bc866275e2af906bed0ef506f7b017fc
NardeenMahfouzFakhrey <1900586@eng.asu.edu.eg>
2023-04-13 03:18:39 +0200

    RR

--- OS_Project/src/main/java/com/example/os_project/RoundRobin.java
+++ OS_Project/src/main/java/com/example/os_project/RoundRobin.java
@@ -4,25 +4,72 @@ import java.util.ArrayList;
 
 public class RoundRobin extends Algorithm {
 
+    ArrayList<Burst> burst = new ArrayList<Burst>();
+    ArrayList<Process> process;
+    int number_of_process;
     float avg_waiting_time = 0;
     float avg_turnaround_time = 0;
-    public RoundRobin(int pn, ArrayList<Process> ps) {
+    int quantumTime;
+
+    public RoundRobin(int pn, ArrayList<Process> ps, int quantumTime) {
         super(pn, ps);
+        this.quantumTime=quantumTime;
+        this.process=ps;
+        this.number_of_process=pn;
     }
 
     @Override
     public ArrayList<Burst> schedule(String algo_Type){
+        int currentTime=0;
+        int actualQuantum=quantumTime;
+        boolean flag=false;
+        while (process.size()>0) {
+            for (int i = 0; i < process.size(); i++) {
 
-        return null;
+                if (process.get(i).getAr() <= currentTime ) {
+                    flag=true;
+                    if (process.get(i).getRt() >= quantumTime) {
+
+                        process.get(i).setRt(process.get(i).getRt() - quantumTime);
+                        burst.add(new Burst(process.get(i), quantumTime));
+                        if (process.get(i).getRt() == 0) {
+                            System.out.println("curr"+currentTime);
+                            avg_turnaround_time+=currentTime-process.get(i).getAr()+quantumTime;
+                            System.out.println(" av"+avg_turnaround_time);
+                            process.remove(process.get(i));
+                            i--;
+                        }
+                    }
+                    else {
+                        actualQuantum = process.get(i).getRt();
+                        burst.add(new Burst(process.get(i), actualQuantum));
+                        System.out.println( "cur"+currentTime);
+
+                        avg_turnaround_time+=currentTime-process.get(i).getAr()+actualQuantum;
+                        System.out.println("av "+avg_turnaround_time);
+                        process.remove(process.get(i));
+                        i--;
+                    }
+                    currentTime = currentTime + actualQuantum;
+                    actualQuantum = quantumTime;
+                }
+
+            }
+            if(flag==false){
+                currentTime++;
+            }
+        }
+        return burst;
     }
 
     @Override
     public float compute_avgwt() {
-        return avg_waiting_time;
+        return avg_waiting_time/number_of_process;
     }
 
     @Override
     public float compute_avgta() {
-        return avg_turnaround_time;
+        return avg_turnaround_time/number_of_process;
     }
 }
+
